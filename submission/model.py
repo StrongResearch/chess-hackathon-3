545cc281ad85699c7aeffe8172a0cf581e57d072
Ralph Furman <rcf@acm.org>
2024-08-31 20:01:55 +0000

    Fix up submission model

--- submission/model.py
+++ submission/model.py
@@ -1,17 +1,17 @@
 from hackathon_train.train import VisionTransformerTwoHeads
 from torch import nn
 
-class VisionTransformerInference(nn.module):
-    def __init__(self, config):
+class Model(nn.Module):
+    def __init__(self, model):
         super().__init__()
         self.model = VisionTransformerTwoHeads(
-            in_channels=config['model']['in_channels'],
-            patch_size=config['model']['patch_size'],
-            embed_dim=config['model']['embed_dim'],
-            num_heads=config['model']['num_heads'],
-            num_layers=config['model']['num_layers'],
-            num_classes1=config['model']['num_classes1'],
-            num_classes2=config['model']['num_classes2']
+            in_channels=model['in_channels'],
+            patch_size=model['patch_size'],
+            embed_dim=model['embed_dim'],
+            num_heads=model['num_heads'],
+            num_layers=model['num_layers'],
+            num_classes1=model['num_classes1'],
+            num_classes2=model['num_classes2']
         )
         self.criterion1 = nn.MSELoss()
         self.criterion2 = nn.MSELoss()
