7a0d0f0d34c613edc12dac94647a2dda165d341c
StrongAdam <142765790+adam-peaston-SC@users.noreply.github.com>
2024-09-02 10:36:03 +1000

    Update transformer_utils.py

--- utils/transformer_utils.py
+++ utils/transformer_utils.py
@@ -11,13 +11,13 @@ def softmax(x, dim=-1, temp=1, ghost=None):
 
 def multihead_cross_attention(Q, K, V, causal=True, ghost=None, device='cpu'):
     '''
-    Accepts input of Q, K, V each with shape (batch_size, nhead, seq_len, inner_dim),
-    or more generally with shape (..., seq_len, inner_dim).
+    Accepts input of Q, K, V each with shape (batch_size, nhead, seq_len, head_dim),
+    or more generally with shape (..., seq_len, head_dim).
     If causal, causal mask is generated and applied.
-    Returns attention tensor A of shape (..., seq_len, inner_dim).
+    Returns attention tensor A of shape (..., seq_len, head_dim).
     '''
-    _batch_size, _nhead, seq_len, inner_dim = Q.shape
-    QKT = torch.einsum('...Qe,...Ke->...QK', Q, K) / math.sqrt(inner_dim)
+    _batch_size, _nhead, seq_len, head_dim = Q.shape
+    QKT = torch.einsum('...Qe,...Ke->...QK', Q, K) / math.sqrt(head_dim)
     if causal:
         mask = nn.Transformer.generate_square_subsequent_mask(seq_len, dtype=torch.float, device=device)
         mask = mask.view(1, 1, seq_len, seq_len)
@@ -31,17 +31,17 @@ class MultiHeadSelfAttention(nn.Module):
     Assumes input with shape (batch_size, seq_len, embed_dim).
     If causal, causal mask is generated and applied.
     '''
-    def __init__(self, embed_dim=512, nhead=8, inner_dim=512, causal=True, ghost=False, device='cpu'):
+    def __init__(self, embed_dim=512, nhead=8, head_dim=64, causal=True, ghost=False, device='cpu'):
         super().__init__()
         self.nhead = nhead
-        self.inner_dim = inner_dim
+        self.head_dim = head_dim
         self.causal = causal
         self.ghost = ghost
         self.device = device
         # ghost is one learnable param per attention head
         self.ghost = nn.parameter.Parameter(data=torch.zeros(nhead)) if ghost else None
-        self.Wqkv = nn.Linear(embed_dim, 3 * nhead * inner_dim)
-        self.Wo = nn.Linear(nhead * inner_dim, embed_dim)
+        self.Wqkv = nn.Linear(embed_dim, 3 * nhead * head_dim)
+        self.Wo = nn.Linear(nhead * head_dim, embed_dim)
         self.init_weights()
 
     def init_weights(self):
@@ -53,7 +53,7 @@ class MultiHeadSelfAttention(nn.Module):
     def forward(self, inputs):
         batch_size, seq_len, embed_dim = inputs.shape
         QKV = self.Wqkv(inputs)
-        QKVh = QKV.reshape(batch_size, seq_len, 3, self.nhead, self.inner_dim).transpose(1, 3)
+        QKVh = QKV.reshape(batch_size, seq_len, 3, self.nhead, self.head_dim).transpose(1, 3)
         Q, K, V = [t.squeeze(2) for t in QKVh.split(1, 2)] # squeezing out the projection dimension only
         A = multihead_cross_attention(Q, K, V, causal=self.causal, ghost=self.ghost, device=self.device).transpose(1, 2).reshape(batch_size, seq_len, -1)
         outputs = self.Wo(A)
@@ -77,10 +77,10 @@ class FeedForward(nn.Module):
         return self.lin2(self.act(self.lin1(inputs)))
 
 class TransformerEncoderBlock(nn.Module):
-    def __init__(self, embed_dim=512, nhead=8, inner_dim=512, ff_dim=2048, dropout=0.1, causal=True, norm_first=False, ghost=False, device='cpu'):
+    def __init__(self, embed_dim=512, nhead=8, head_dim=64, ff_dim=2048, dropout=0.1, causal=True, norm_first=False, ghost=False, device='cpu'):
         super().__init__()
         self.norm_first = norm_first
-        self.self_attention = MultiHeadSelfAttention(embed_dim, nhead, inner_dim, causal, ghost, device)
+        self.self_attention = MultiHeadSelfAttention(embed_dim, nhead, head_dim, causal, ghost, device)
         self.norm1 = nn.LayerNorm(embed_dim)
         self.feedforward = FeedForward(embed_dim, ff_dim)
         self.norm2 = nn.LayerNorm(embed_dim)
